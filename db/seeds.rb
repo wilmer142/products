9b3eb988fbc2c619095f19312d08eb43b0c317f1
wilmer142 <wilmercollege@gmail.com>
2019-01-29 22:23:06 -0500

    Modify routes and add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Category.create(name: "Perfumes")
+Category.create(name: "Libros")
+Category.create(name: "Tecnología")
+Category.create(name: "Electrodomésticos")
+Category.create(name: "Aseo")
+Category.create(name: "Juguetes")
+Category.create(name: "Ropa")
+Category.create(name: "Oficina")
+Category.create(name: "Mascotas")
