894c076c80ea74bc0337aacb6aaaf4ecfae56fc6
wilmer142 <wilmercollege@gmail.com>
2019-01-29 21:55:10 -0500

    Add create method for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,23 @@
 class ProductsController < ApplicationController
   def index
+  	@products = Product.all
   end
 
   def new
+  	@product = Product.new
   end
 
-  def edit
+  def create
+  	@product = Product.new(product_params)
+  	if @product.save
+  		redirect_to products_path, notice: "El producto fue creado con éxito"
+  	else 
+  		render :new
+  	end
   end
+
+  private 
+  	def product_params
+  	 	params.require(:product).permit(:name, :price, category_ids:[])
+  	 end 
 end
